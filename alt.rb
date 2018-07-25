3627a567541ca8e663dc87f57cc90ea16b1c71df
monet-jones <monet.s.jones@gmail.com>
2018-07-25 18:52:02 +0000

    automatically backed up by learn

--- alt.rb
+++ alt.rb
@@ -4,8 +4,11 @@ def least_coins(cents)
   num_quarters = cents / 25 
   solution [:quarters]= num_quarters 
   
+  num_dimes = cents / 10
+  solution [:dimes]=num_dimes
+  
   puts solution
   
 end 
 
-least_coins(50)
+least_coins(70)
