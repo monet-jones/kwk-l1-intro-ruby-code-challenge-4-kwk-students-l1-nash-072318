cdd432a1ff2c0dc5de9ecdad08ce75a70085e883
monet-jones <monet.s.jones@gmail.com>
2018-07-25 18:37:08 +0000

    automatically backed up by learn

--- least_coins.rb
+++ least_coins.rb
@@ -2,7 +2,22 @@
 
 def least_coins=117
 coins={"Quarters"=> 0, "dimes"=> 0, "nickles"=> 0, "pennies"=> 0 }
-puts Quarters 
+If  "pennies"<5
+puts "pennies"=1
+
+elsif "nickles" = 5
+puts "nickles"=1
+
+elsif "dimes"= 10
+puts "dimes"=1
+
+else "Quarters"=25
+  puts "Quarters" =1
+  
+end 
+
+least_coins
+  
 
   
   
