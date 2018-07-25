12214ebe404f826ddc95ab313abf8738c59667a4
monet-jones <monet.s.jones@gmail.com>
2018-07-25 18:26:51 +0000

    automatically backed up by learn

--- least_coins.rb
+++ least_coins.rb
@@ -1,7 +1,9 @@
 #write out your code here
 
 def least_coins=117
-{"Quarters"=> 0, "dimes"=> 0, "nickles"=> 0, "pennies"=> 0"}
+coins={"Quarters"=> 0, "dimes"=> 0, "nickles"=> 0, "pennies"=> 0 }
+puts Quarters 
+
   
   
 
